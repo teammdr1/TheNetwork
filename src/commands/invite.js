2ed714bbbd436aeddfb7188d2b001ae952c0d909
teammdr <141670764+teammdr1@users.noreply.github.com>
2026-04-18 09:46:05 +0200

    Create invite.js

---
+++ src/commands/invite.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  description: "Invite le bot sur votre serveur",
+
+  async execute(client, message, args) {
+    const inviteEmbed = new EmbedBuilder()
+      .setTitle("Invite le bot")
+      .setDescription(
+        `Clique sur le lien suivant pour inviter le bot sur ton serveur : https://discord.com/oauth2/authorize?client_id=1492793436821131394&permissions=8&integration_type=0&scope=bot`,
+      )
+      .setColor("#464ec2");
+
+    message.reply({ embeds: [inviteEmbed] });
+  },
+};
