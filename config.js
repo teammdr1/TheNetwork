0a13bba07aac2407cc2c641c2a8cbaeb429e8b2f
teammdr <141670764+teammdr1@users.noreply.github.com>
2026-05-02 19:40:27 +0200

    Update prefix and support server invite in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: process.env.DISCORD_TOKEN || 'ton-token-ici',
-  prefix: '+',
+  prefix: '+', // préfix par défaut
   embedColor: '#49ff02',
   ownerId: process.env.OWNER_ID || 'ton-id-ici',
-  supportServerInvite: 'https://discord.gg/tqxJPf4YFc'
+  supportServerInvite: 'lien-de-ton-serveur'
 };
