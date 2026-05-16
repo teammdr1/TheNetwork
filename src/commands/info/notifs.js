6b0acbd3eba3eecf486069bb6292489f9a123bce
teammdr <141670764+teammdr1@users.noreply.github.com>
2026-05-16 09:04:13 +0200

    Update notifs.js

--- src/commands/info/notifs.js
+++ src/commands/info/notifs.js
@@ -1,36 +1,16 @@
-const {
-    MessageFlags,
-    ContainerBuilder,
-    TextDisplayBuilder,
-    SeparatorBuilder,
-    SectionBuilder,
-    ThumbnailBuilder,
-} = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'notifs',
     description: 'Affiche un message concernant les rôles du serveur',
     async execute(client, message, args) {
-        const container = new ContainerBuilder().setAccentColor(0xFFFB00);
-
-        container.addSectionComponents(
-            new SectionBuilder()
-                .addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent(
-                        `## 🎭 Obtenir des rôles — ${message.guild.name}`
-                    )
-                )
-                .setThumbnailAccessory(
-                    new ThumbnailBuilder().setURL(message.guild.iconURL({ dynamic: true, size: 128 }) || 'https://cdn.discordapp.com/embed/avatars/0.png')
-                )
-        );
-        container.addSeparatorComponents(new SeparatorBuilder().setSpacing(1).setDivider(true));
-        container.addTextDisplayComponents(
-            new TextDisplayBuilder().setContent(
-                'Pour modifier vos rôles, rendez-vous dans **Channels & Roles** : <id:customize>\n\nCliquez sur les différentes options pour obtenir les rôles qui vous correspondent !'
-            )
-        );
-
-        message.channel.send({ components: [container], flags: MessageFlags.IsComponentsV2 });
+        const iconURL = message.guild.iconURL({ dynamic: true, size: 256})
+        const embed = new EmbedBuilder()
+            .setColor('#fffb00')
+            .setTitle('🎭 ・Comment avoir des rôles sur le serveur ?')
+            .setDescription('Pour obtenir des rôles sur le serveur, il suffit d\'aller dans le menu <id:customize> et de cliquer sur les rôles qui vous correspondent le mieux.')
+            .setImage('https://cdn.discordapp.com/attachments/1505102292670615572/1505102690471116932/image.png?ex=6a09679c&is=6a08161c&hm=a951bd51abf8c627ae220166d2c91a3badaf1bec6b30cfab7e5acabc09faecca&')
+            .setThumbnail(iconURL)
+        message.channel.send({ embeds: [embed] });
     }
 };
